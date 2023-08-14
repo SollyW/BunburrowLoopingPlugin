b92772b306255ed482c274a94db956d86800b56c
Solly Watkins <solly@sollyw.com>
2023-08-14 15:43:33 +1000

    Initial commit

---
+++ BunburrowLoopingPlugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Levels;
+using Misc;
+
+namespace BunburrowLoopingPlugin;
+
+[BepInPlugin("au.sollyw.bunburrowlooping", "Bunburrow Looping", "1.0.0")]
+public class BunburrowLoopingPlugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(BunburrowLoopingPlugin));
+    }
+
+    [HarmonyPatch(typeof(LevelsList))]
+    [HarmonyPatch("AdjacentBunburrows", MethodType.Getter)]
+    [HarmonyPostfix]
+    public static void AdjacentBunburrows(ref DirectionsListOf<LevelsList> __result)
+    {
+        DirectionsListOf<LevelsList> copy = new DirectionsListOf<LevelsList>(
+            __result[Direction.Left],
+            __result[Direction.Up],
+            __result[Direction.Right],
+            __result[Direction.Down]);
+        __result = copy;
+        FillVoids(copy, Direction.Left);
+        FillVoids(copy, Direction.Up);
+        FillVoids(copy, Direction.Right);
+        FillVoids(copy, Direction.Down);
+
+        static void FillVoids(DirectionsListOf<LevelsList> levelsLists, Direction direction) {
+            LevelsList levelList = levelsLists[direction];
+            if (levelList == null) {
+                Direction opposite = direction.GetOpposite();
+                levelsLists.SetPart(direction,
+                    levelsLists[opposite]?.AdjacentBunburrows[opposite]);
+                UnityEngine.Debug.Log($"Replaced {direction} with {levelsLists[direction]}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Installing
+See discord pins
+
+# Compiling
+Place a copy of the game's Assembly-CSharp.dll file into the lib directory, then run `dotnet build`
